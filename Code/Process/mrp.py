810e0a75a2e3b1860f5ca43978b4c0e88984f322
Yu Gu <32755499+guyu980@users.noreply.github.com>
2019-01-27 20:35:25 -0800

    Update mrp.py

--- Code/Process/mrp.py
+++ Code/Process/mrp.py
@@ -5,24 +5,23 @@ from utils.generic_typevars import S
 
 class MRP(MP):
     def __init__(self, transitions: Mapping[S, Mapping[S, float]], \
-                 rewards: Mapping[S, Mapping[S, float]], gamma: float):
+                 rewards: Mapping[S, float], gamma: float):
         if self.check_mp(transitions):
             self.transitions = transitions
-            self.states = list(self.transitions.keys())
+            self.states = self.get_all_states()
             self.rewards = rewards
+            self.reward_func = self.get_reward_func()
             self.transition_matrix = self.generate_matrix(self.transitions)
-            self.reward_func = self.generate_matrix(self.rewards)
+            self.gamma = gamma
         else:
             raise ValueError
     
-    def convert_reward_func(self):
-        reward_func_new = np.zeros(len(self.states))
-        
-        for i in range(len(self.states)):
-            reward_func_new[i] = np.dot(self.transition_matrix[i, :], self.reward_func[i, :])
-        return reward_func_new
+    def get_reward_func(self):
+        return list(self.rewards.values())
     
-
+    def get_value_func(self) -> np.array:
+        return np.linalg.inv(np.eye(len(self.states)) - self.gamma * self.transition_matrix \
+                             ).dot(self.reward_func)
 
 if __name__ == '__main__':
     transitions = {
@@ -33,14 +32,10 @@ if __name__ == '__main__':
         5: {5: 1}
     }
     rewards = {
-        1: {1: 3, 2: 8, 3: 10, 4: 5, 5: 3},
-        2: {1: 2, 2: 7, 3: 9, 5: 2},
-        3: {1: 0, 2: 1, 4: 3},
-        4: {1: 0, 2: 4, 3: 5, 5: 2},
-        5: {5: 1}
+        1: 8.4, 2: 8.4, 3: 5, 4: 6.8, 5: 2
     }
-    mrp = MRP(transitions, rewards, 1.)
+    mrp = MRP(transitions, rewards, 0.5)
     print('States:', mrp.states, '\n')
     print('Transition Matrix:\n', mrp.transition_matrix, '\n')
     print('Reward Function (Wiki):\n', mrp.reward_func, '\n')
-    print('Reward Function (DS):\n', mrp.convert_reward_func(), '\n')
+    print('Value Function:\n', mrp.get_value_func(), '\n')
