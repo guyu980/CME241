86a4d9152766073b9ac1c40680c8280dc32138fd
Yu Gu <32755499+guyu980@users.noreply.github.com>
2019-01-27 20:34:39 -0800

    Update utils.py

--- Code/Process/utils/utils.py
+++ Code/Process/utils/utils.py
@@ -1,4 +1,12 @@
+from typing import Mapping, TypeVar, Sequence
+
+X = TypeVar('X')
+
 eps = 1e-8
 
 def is_equal(a: float, b:float) -> bool:
     return abs(a - b) <= eps
+
+def sum_dicts(dicts: Sequence[Mapping[X, float]]) -> Mapping[X, float]:
+    return {k: sum(d.get(k, 0.) for d in dicts)
+            for k in set.union(*[set(d1) for d1 in dicts])}
