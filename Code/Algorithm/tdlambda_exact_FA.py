70aef7cf4aa94ce9ac860a9fbf321dc261490e7c
Yu Gu <32755499+guyu980@users.noreply.github.com>
2019-03-21 22:59:03 -0700

    Update tdlambda_exact_FA.py

--- Code/Algorithm/tdlambda_exact_FA.py
+++ Code/Algorithm/tdlambda_exact_FA.py
@@ -1,11 +1,9 @@
 from typing import Callable, Tuple, Optional, Sequence, Mapping
-from algorithms.td_algo_enum import TDAlgorithm
-from algorithms.rl_func_approx.rl_func_approx_base import RLFuncApproxBase
-from algorithms.func_approx_spec import FuncApproxSpec
-from processes.mdp_rep_for_rl_fa import MDPRepForRLFA
-from processes.mp_funcs import get_rv_gen_func_single
-from algorithms.helper_funcs import get_soft_policy_func_from_qf
-from processes.mp_funcs import get_expected_action_value
+from td_algo_enum import TDAlgorithm
+from rl_func_approx.rl_func_approx_base import RLFuncApproxBase
+from func_approx_spec import FuncApproxSpec
+from Process.mdp_rep_for_rl_fa import MDPRepForRLFA
+from helper_funcs import get_soft_policy_func_from_qf
 import numpy as np
 from utils.generic_typevars import S, A
 from utils.standard_typevars import VFType, QFType, PolicyActDictType
@@ -110,11 +108,6 @@ class TDLambdaExact(RLFuncApproxBase):
                 state = self.mdp_rep.init_state_gen()
                 action = get_rv_gen_func_single(this_polf(state))()
             features = self.qvf_fa.get_feature_vals((state, action))
-
-            # print((episodes, max(self.qvf_fa.get_feature_vals((state, a)).dot(self.qvf_w)
-            #                      for a in self.mdp_rep.state_action_func(state))))
-            # print(self.qvf_w)
-
             old_qvf_fa = 0.
             steps = 0
             terminate = False
@@ -130,9 +123,6 @@ class TDLambdaExact(RLFuncApproxBase):
                         (next_state, a)).dot(self.qvf_w) for a in
                                       self.state_action_func(next_state))
                 elif self.algorithm == TDAlgorithm.ExpectedSARSA and control:
-                    # next_qvf_fa = sum(this_polf(next_state).get(a, 0.) *
-                    #               self.qvf_fa.get_feature_vals((next_state, a)).dot(self.qvf_w)
-                    #               for a in self.state_action_func(next_state))
                     next_qvf_fa = get_expected_action_value(
                         {a: self.qvf_fa.get_feature_vals((next_state, a)).dot(self.qvf_w)
                          for a in self.state_action_func(next_state)},
@@ -180,89 +170,3 @@ class TDLambdaExact(RLFuncApproxBase):
 
         return lambda st: lambda act, st=st: self.qvf_fa.get_feature_vals(
             (st, act)).dot(self.qvf_w)
-
-
-if __name__ == '__main__':
-    from processes.mdp_refined import MDPRefined
-    mdp_refined_data = {
-        1: {
-            'a': {1: (0.3, 9.2), 2: (0.6, 4.5), 3: (0.1, 5.0)},
-            'b': {2: (0.3, -0.5), 3: (0.7, 2.6)},
-            'c': {1: (0.2, 4.8), 2: (0.4, -4.9), 3: (0.4, 0.0)}
-        },
-        2: {
-            'a': {1: (0.3, 9.8), 2: (0.6, 6.7), 3: (0.1, 1.8)},
-            'c': {1: (0.2, 4.8), 2: (0.4, 9.2), 3: (0.4, -8.2)}
-        },
-        3: {
-            'a': {3: (1.0, 0.0)},
-            'b': {3: (1.0, 0.0)}
-        }
-    }
-    gamma_val = 0.9
-    mdp_ref_obj1 = MDPRefined(mdp_refined_data, gamma_val)
-    mdp_rep_obj = mdp_ref_obj1.get_mdp_rep_for_rl_tabular()
-
-    exploring_start_val = False
-    algorithm_type = TDAlgorithm.ExpectedSARSA
-    softmax_flag = False
-    epsilon_val = 0.1
-    epsilon_half_life_val = 1000
-    learning_rate_val = 0.1
-    learning_rate_decay_val = 1e6
-    lambda_val = 0.7
-    episodes_limit = 10000
-    max_steps_val = 1000
-    state_ff = [lambda s: float(s)]
-    sa_ff = [
-        lambda x: float(x[0]),
-        lambda x: 1. if x[1] == 'a' else 0.,
-        lambda x: 1. if x[1] == 'b' else 0.,
-        lambda x: 1. if x[1] == 'c' else 0.,
-    ]
-    esl_obj = TDLambdaExact(
-        mdp_rep_for_rl=mdp_rep_obj,
-        exploring_start=exploring_start_val,
-        algorithm=algorithm_type,
-        softmax=softmax_flag,
-        epsilon=epsilon_val,
-        epsilon_half_life=epsilon_half_life_val,
-        lambd=lambda_val,
-        num_episodes=episodes_limit,
-        max_steps=max_steps_val,
-        state_feature_funcs=state_ff,
-        sa_feature_funcs=sa_ff,
-        learning_rate=learning_rate_val,
-        learning_rate_decay=learning_rate_decay_val
-    )
-
-    def policy_func(i: int) -> Mapping[str, float]:
-        if i == 1:
-            ret = {'a': 0.4, 'b': 0.6}
-        elif i == 2:
-            ret = {'a': 0.7, 'c': 0.3}
-        elif i == 3:
-            ret = {'b': 1.0}
-        else:
-            raise ValueError
-        return ret
-
-    this_qf = esl_obj.get_qv_func_fa(policy_func)
-    this_vf = esl_obj.get_value_func_fa(policy_func)
-    print(this_vf(1))
-    print(this_vf(2))
-    print(this_vf(3))
-
-    opt_det_polf = esl_obj.get_optimal_det_policy_func()
-
-    # noinspection PyShadowingNames
-    def opt_polf(s: S, opt_det_polf=opt_det_polf) -> Mapping[A, float]:
-        return {opt_det_polf(s): 1.0}
-
-    opt_vf = esl_obj.get_value_func_fa(opt_polf)
-    print(opt_polf(1))
-    print(opt_polf(2))
-    print(opt_polf(3))
-    print(opt_vf(1))
-    print(opt_vf(2))
-    print(opt_vf(3))
